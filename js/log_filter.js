29659cc4c8786949de72ff9d1fb2fbe461e2f69e
jacobfriis <jacob.friis@simple-complex.net>
2014-04-27 14:03:13 +0200

    see next commit for details

--- js/log_filter.js
+++ js/log_filter.js
@@ -2815,13 +2815,11 @@
               _.adminOverlayOffset,
             1
           );
-
           // Format inspect outputs.
           if (typeof window.inspect === "function" && window.inspect.formatInspectOutput) {
             inspect.formatInspectOutput(true, dialInner);
             inspect.formatTraceOutput(true, dialInner);
           }
-
           $dialOuter.css({
             visibility: 'visible',
             left: '150px', // jQuery UI dialog position apparantly doesnt work well when css position is fixed.
